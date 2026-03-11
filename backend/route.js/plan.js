37b62b60abfb6cf51b6bfe2a0441570ad3e33785
ai208 <yasu6328hand@gmail.com>
2026-03-11 16:31:45 +0900

    Intial commit

---
+++ backend/route.js/plan.js
@@ -0,0 +1,2 @@
+//URL と Controller の　橋渡しをする
+// url → routine → controller 
---
+++ backend/route.js/weights.js
@@ -0,0 +1,3 @@
+// URL と　Controller の橋渡しをする
+// ex /weight への　POST や GET をどの Controller に渡すかを定義する
+// url → route → controller
